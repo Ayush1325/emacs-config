4fd14efb6d866c74ea915bf1f2daa47711bbb7f5
Ayush Singh <ayushsingh1325@gmail.com>
2021-09-13 10:26:51 +0530

    feat: Added Company Mode

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -6,7 +6,7 @@
  '(global-display-line-numbers-mode t)
  '(org-hide-emphasis-markers t)
  '(package-selected-packages
-   '(centaur-tabs treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil dap-mode math-preview rainbow-delimiters rustic lsp-ivy lsp-ui lsp-mode pdf-tools org-roam json-mode yaml-mode vterm smartparens highlight-defined magit projectile yasnippet move-text hungry-delete undo-tree iedit multiple-cursors expand-region all-the-icons-ivy-rich counsel ivy which-key page-break-lines dashboard hide-mode-line doom-modeline evil-collection evil doom-themes toc-org org-bullets use-package))
+   '(company-fuzzy company treemacs centaur-tabs treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil dap-mode math-preview rainbow-delimiters rustic lsp-ivy lsp-ui lsp-mode pdf-tools org-roam json-mode yaml-mode vterm smartparens highlight-defined magit projectile yasnippet move-text hungry-delete undo-tree iedit multiple-cursors expand-region all-the-icons-ivy-rich counsel ivy which-key page-break-lines dashboard hide-mode-line doom-modeline evil-collection evil doom-themes toc-org org-bullets use-package))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
